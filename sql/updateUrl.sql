02a098984b3d94517f184b28c68837710e4b3194
abaguio <aerogaming@ymail.com>
2019-01-17 07:23:33 -0500

    FIX: Fixed updating the sql file. So it will update the selected database instead of autotrader.

--- sql/updateUrl.sql
+++ sql/updateUrl.sql
@@ -1,2 +1,2 @@
-UPDATE `autotrader`.`wp_options` SET `option_id`='1', `option_name`='siteurl', `option_value`='http://autotrader.local/', `autoload`='yes' WHERE (`option_id`='1');
-UPDATE `autotrader`.`wp_options` SET `option_id`='2', `option_name`='home', `option_value`='http://autotrader.local/', `autoload`='yes' WHERE (`option_id`='2');
+UPDATE wp_options set `option_value` = 'http://autotrader.local/' WHERE (`option_id`='1');
+UPDATE wp_options set `option_value` = 'http://autotrader.local/' WHERE (`option_id`='2');
